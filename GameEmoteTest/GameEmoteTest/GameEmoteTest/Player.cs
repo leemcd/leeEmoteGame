ee4691b69d50ddba94b961d34009e04f56955b3d
Lee McDonald <omnirend@yahoo.co.uk>
2012-07-07 10:31:29 +0100

    Created the Main Player class
    Added the class that will contain all the player information. Just basic
    class stub at this time.

---
+++ GameEmoteTest/GameEmoteTest/GameEmoteTest/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameEmoteTest
+{
+    class Player
+    {
+        public Player()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
